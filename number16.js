69ae3e79a56a782118a458508dccf5f0822e6839
Steve Cirelli <scirelli+git@gmail.com>
2019-09-04 18:28:30 -0400

    number16

--- number16.js
+++ number16.js
@@ -2,3 +2,32 @@
 /*
  * Given string str, How do you find the longest palindromic substring in str?
  */
+
+function longestPalindromicSubstring(str) {
+    let longestPalindrom = '', allPalindroms = [];
+
+    for(let i=0; i<str.length; i++) {
+        for(let j=str.length, ss; j>i; j--) {
+            ss = str.substring(i, j);
+            if(isPalindrom(ss)) {
+                allPalindroms.push(ss);
+            }
+        }
+    }
+
+    allPalindroms.forEach((str)=> {
+        if(str.length > longestPalindrom.length) {
+            longestPalindrom = str;
+        }
+    });
+
+    return longestPalindrom;
+    function isPalindrom(str) {
+        str = str.toLowerCase();
+        return str === str.split('').reverse().join('');
+    }
+}
+
+['aaa', 'aaabaaaa', 'ababab', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz', 'abcdefghijklaaaamnopqrstuvwxyzabcdefghpalindromsmordnilapijklmnopqrstuvwxyz'].forEach((str)=> {
+    console.log(longestPalindromicSubstring(str));
+});
